2aaa3f73258241c8a528b69562a48b151b046dc5
Fajrin Rama Sanjaya <fajrinramasanjaya@yahoo.com>
2022-10-29 09:09:26 +0700

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   src/components/information/information.jsx
    	modified:   src/components/tabsContent/history.jsx

--- src/components/information/information.jsx
+++ src/components/information/information.jsx
@@ -52,18 +52,18 @@ const Information= () => {
 
       <ids-layout-grid rows="1" gap="md" cols="1">
 
-          <ids-layout-grid-cell>
-            <ids-card>
-              <div slot="card-header">
-                <h2 class="card-title">Newest Documents In Progress</h2>
-              </div>
-              <div slot="card-content">
-                <ListNewestDoc />
-              </div>
-            </ids-card>
-          </ids-layout-grid-cell>
+        <ids-layout-grid-cell>
+          <ids-card>
+            <div slot="card-header">
+              <h2 class="card-title">Newest Documents In Progress</h2>
+            </div>
+            <div slot="card-content">
+              <ListNewestDoc />
+            </div>
+          </ids-card>
+        </ids-layout-grid-cell>
 
-        </ids-layout-grid></>
+      </ids-layout-grid></>
     );
 };
 
